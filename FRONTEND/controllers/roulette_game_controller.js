5db45329dfbffbaf5eacb4fcc9d997b7863392e9
Alan Varela <alanvareli@gmail.com>
2025-04-29 19:08:30 -0600

    Primer commit ruleta
    Falta algunas partes de la interfaz y responsividad

---
+++ FRONTEND/controllers/roulette_game_controller.js
@@ -0,0 +1,210 @@
+// Variables globales de apuestas
+let selectedChipValue = 0;
+let selectedChipColor = '';
+let balance = 1000;
+let totalBet = 0;
+let winnings = 0;
+let placedBets = {}; // Relacion de apuestas
+let chipCounter = 0;
+let placedChipsStack = []; // Pila de fichas colocadas
+
+const numRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+const blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateUI();
+
+  window.onRouletteWinner = (winner) => {
+    if (!winner) return;
+    processWinner(winner);
+    enableSpinButton();
+  };
+
+  document.getElementById("spinButton")?.addEventListener("click", () => {
+    if (pause) {
+      spin();
+      disableSpinButton();
+    }
+  });
+
+  document.getElementById("undoButton")?.addEventListener("click", undoLastBet);
+  document.getElementById("resetButton")?.addEventListener("click", resetAllBets);
+
+  document.querySelectorAll(".chip").forEach((chip) => {
+    chip.addEventListener("click", () => {
+      selectedChipValue = parseInt(chip.dataset.value);
+      selectedChipColor = window.getComputedStyle(chip).backgroundColor;
+    });
+  });
+
+  document.querySelectorAll("td.cell").forEach((cell) => {
+    cell.addEventListener("click", () => {
+      const name = cell.dataset.name;
+      if (!name || selectedChipValue === 0) return;
+      if ( balance < selectedChipValue)
+      {
+         alert("No tienes suficiente balance!")
+         return;
+      }
+
+      balance -= selectedChipValue;
+      totalBet += selectedChipValue;
+      placedBets[name] = (placedBets[name] || 0) + selectedChipValue;
+
+      const chip = document.createElement("div");
+      chip.classList.add("chip", "placed-chip");
+      chip.textContent = selectedChipValue;
+      chip.style.position = "absolute";
+      chip.style.zIndex = 100 + chipCounter++;
+      chip.style.background = selectedChipColor;
+      chip.style.top = `${cell.clientHeight / 2 - 12 + Math.floor(Math.random() * 8 - 4)}px`;
+      chip.style.left = `${cell.clientWidth / 2 - 12 + Math.floor(Math.random() * 8 - 4)}px`;
+      cell.appendChild(chip);
+
+      placedChipsStack.push({ chip, cell, value: selectedChipValue, name });
+      updateUI();
+    });
+  });
+});
+
+function updateUI() {
+  document.getElementById("balanceAmount").textContent = `$${balance}`;
+  document.getElementById("totalBet").textContent = `$${totalBet}`;
+  document.getElementById("winningAmount").textContent = `$${winnings}`;
+}
+
+function processWinner(winner) {
+  const result = winner.number;
+  const color = winner.color.toLowerCase();
+  console.log(`Numero ganador: ${result}, Color: ${color}`);
+  calculateWinnings(result, color);
+  showResult(result);
+}
+
+function calculateWinnings(result, color) {
+  let payout = 0;
+
+  for (const [key, amount] of Object.entries(placedBets)) {
+    const num = parseInt(key);
+    if (!isNaN(num) && num === result) payout += amount * 36;
+    else if (key === "red" && color === "#8b0000") payout += amount * 2;
+    else if (key === "black" && color === "#0a0a0a") payout += amount * 2;
+    else if (key === "green" && color === "#064420") payout += amount * 36;
+    else if (key === "even" && result !== 0 && result % 2 === 0) payout += amount * 2;
+    else if (key === "odd" && result % 2 === 1) payout += amount * 2;
+    else if (key === "1-18" && result >= 1 && result <= 18) payout += amount * 2;
+    else if (key === "19-36" && result >= 19 && result <= 36) payout += amount * 2;
+    else if (key === "1st12" && result >= 1 && result <= 12) payout += amount * 3;
+    else if (key === "2nd12" && result >= 13 && result <= 24) payout += amount * 3;
+    else if (key === "3rd12" && result >= 25 && result <= 36) payout += amount * 3;
+    else if (key === "2to1-top" && [3,6,9,12,15,18,21,24,27,30,33,36].includes(result)) payout += amount * 3;
+    else if (key === "2to1-middle" && [2,5,8,11,14,17,20,23,26,29,32,35].includes(result)) payout += amount * 3;
+    else if (key === "2to1-bottom" && [1,4,7,10,13,16,19,22,25,28,31,34].includes(result)) payout += amount * 3;
+  }
+
+  winnings = payout;
+  balance += payout;
+  placedBets = {};
+  totalBet = 0;
+
+  document.querySelectorAll(".placed-chip").forEach(ch => ch.remove());
+  placedChipsStack = []; // Limpia la pila también
+  updateUI();
+}
+
+function showResult(result) {
+  const overlay = document.createElement("div");
+  overlay.innerHTML = `
+    <div style="font-size: 8rem; font-weight: bold; ${winnings > 0 ? "color: lightgreen;" : "color: white;"} text-shadow: 2px 2px 10px black;">
+      ${result}
+    </div>
+    ${
+      winnings > 0
+        ? `<div style="font-size: 3rem; margin-top: 10px; color: lightgreen; font-weight: bold;">+ $${winnings}</div>`
+        : ""
+    }
+  `;
+
+  Object.assign(overlay.style, {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(0,0,0,0.7)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    zIndex: 9999,
+    pointerEvents: "none"
+  });
+
+  document.body.appendChild(overlay);
+
+  setTimeout(() => overlay.remove(), 3000);
+  mostrarGanadores(result);
+}
+
+function disableSpinButton() {
+  document.getElementById("spinButton")?.setAttribute("disabled", "true");
+}
+
+function enableSpinButton() {
+  document.getElementById("spinButton")?.removeAttribute("disabled");
+}
+
+function undoLastBet() {
+  if (placedChipsStack.length === 0) return;
+
+  const last = placedChipsStack.pop();
+  last.chip.remove();
+
+  balance += last.value;
+  totalBet -= last.value;
+  placedBets[last.name] -= last.value;
+
+  if (placedBets[last.name] <= 0) delete placedBets[last.name];
+
+  updateUI();
+}
+
+function resetAllBets() {
+  // Sumar todo el dinero apostado de regreso al balance
+  for (const amount of Object.values(placedBets)) {
+    balance += amount;
+  }
+
+  // Borrar las fichas visuales
+  placedChipsStack.forEach(({ chip }) => chip.remove());
+
+  // Limpiar todo
+  placedChipsStack = [];
+  placedBets = {};
+  totalBet = 0;
+  
+  // Actualizar la UI
+  updateUI();
+}
+
+const ganadores = [];
+
+function mostrarGanadores(n) {
+  ganadores.unshift(n); // Agrega el nuevo al inicio
+  if (ganadores.length > 5) ganadores.pop(); // Máximo 4 números
+
+  const container = document.getElementById("ganadoresList");
+  if (!container) return;
+
+  container.innerHTML = "";
+
+  ganadores.forEach(num => {
+    const item = document.createElement("div");
+    const colorClass = num === 0 ? "green" : (numRed.includes(num) ? "red" : "black");
+  
+    item.className = `ganador-item ${colorClass}`;
+    item.innerHTML = `<span class="number">${num}</span>`;
+    container.appendChild(item);
+  });
+}
---
+++ FRONTEND/controllers/wheel_controller.js
@@ -0,0 +1,163 @@
+// Configuración del canvas
+const canvas = document.getElementById("rouletteCanvas");
+const ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
+const centerX = width / 2;
+const centerY = height / 2;
+const radius = width / 2;
+
+//global
+// Creamos un "listener" de ganador
+window.onRouletteWinner = null; // global para que el otro script lo vea
+
+// Números de la ruleta
+const rouletteNumbers = [
+  { number: 0, color: '#064420' }, // Verde especial
+  { number: 32, color: '#8B0000' }, { number: 15, color: '#0A0A0A' }, { number: 19, color: '#8B0000' },
+  { number: 4, color: '#0A0A0A' }, { number: 21, color: '#8B0000' }, { number: 2, color: '#0A0A0A' },
+  { number: 25, color: '#8B0000' }, { number: 17, color: '#0A0A0A' }, { number: 34, color: '#8B0000' },
+  { number: 6, color: '#0A0A0A' }, { number: 27, color: '#8B0000' }, { number: 13, color: '#0A0A0A' },
+  { number: 36, color: '#8B0000' }, { number: 11, color: '#0A0A0A' }, { number: 30, color: '#8B0000' },
+  { number: 8, color: '#0A0A0A' }, { number: 23, color: '#8B0000' }, { number: 10, color: '#0A0A0A' },
+  { number: 5, color: '#8B0000' }, { number: 24, color: '#0A0A0A' }, { number: 16, color: '#8B0000' },
+  { number: 33, color: '#0A0A0A' }, { number: 1, color: '#8B0000' }, { number: 20, color: '#0A0A0A' },
+  { number: 14, color: '#8B0000' }, { number: 31, color: '#0A0A0A' }, { number: 9, color: '#8B0000' },
+  { number: 22, color: '#0A0A0A' }, { number: 18, color: '#8B0000' }, { number: 29, color: '#0A0A0A' },
+  { number: 7, color: '#8B0000' }, { number: 28, color: '#0A0A0A' }, { number: 12, color: '#8B0000' },
+  { number: 35, color: '#0A0A0A' }, { number: 3, color: '#8B0000' }, { number: 26, color: '#0A0A0A' }
+];
+
+// Variables de control
+let currentDeg = 0;
+let speed = 0;
+let maxRotation = 0;
+let pause = true;
+let winnerIndex = null; // Índice del número ganador
+
+//variables estilo
+let blinkState = false;   // Alterna entre dorado y su color original
+let blinkInterval = null; // Guarda el intervalo para parpadeo
+
+// Dibujar ruleta
+function draw() {
+  ctx.clearRect(0, 0, width, height);
+  const step = 360 / rouletteNumbers.length;
+  let startDeg = currentDeg;
+
+  for (let i = 0; i < rouletteNumbers.length; i++, startDeg += step) {
+    const endDeg = startDeg + step;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, radius, toRad(startDeg), toRad(endDeg));
+    ctx.closePath();
+
+    ctx.fillStyle = rouletteNumbers[i].color;
+    ctx.fill();
+
+    ctx.save();
+    ctx.translate(centerX, centerY);
+    ctx.rotate(toRad((startDeg + endDeg) / 2));
+    ctx.textAlign = "right";
+
+    // Definir color del número
+    if (i === winnerIndex) {
+      if (blinkState) {
+        ctx.fillStyle = "#d4af37"; // Dorado cuando parpadea
+      } else {
+        ctx.fillStyle = rouletteNumbers[i].color === "red" ? "red" : "white"; // Su color original
+      }
+    } else {
+      ctx.fillStyle = "white"; // Los demás siempre blancos
+    }
+
+    ctx.font = 'bold 20px sans-serif';
+    ctx.fillText(rouletteNumbers[i].number, radius - 10, 10);
+    ctx.restore();
+    // Círculo negro central
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius * 0.8, 0, Math.PI * 2);  // 10% del radio
+    ctx.fillStyle = "#110";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+// Utilidades
+function toRad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+function easeOutSine(x) {
+  return Math.sin((x * Math.PI) / 2);
+}
+
+function getPercent(input, min, max) {
+  return (((input - min) * 100) / (max - min)) / 100;
+}
+
+// Detectar el número ganador
+function getWinner() {
+  const normalizedDeg = (360 - (currentDeg % 360)) % 360;
+  const step = 360 / rouletteNumbers.length;
+  const index = Math.floor(normalizedDeg / step);
+  const winner = rouletteNumbers[index];
+
+  winnerIndex = index;
+  //console.log(`Ganador: ${winner.number} (${winner.color})`);
+  startBlinking();
+
+  if (typeof window.onRouletteWinner === "function") {
+    window.onRouletteWinner(winner);
+  }
+  return winner;
+ 
+}
+
+// Animar ruleta
+function animate() {
+  if (pause) return;
+
+  speed = easeOutSine(getPercent(currentDeg, maxRotation, 0)) * 20;
+  currentDeg += speed;
+  draw();
+
+  if (speed < 0.01) {
+    speed = 0;
+    pause = true;
+    currentDeg = currentDeg % 360;
+    getWinner();
+    return;
+  }
+
+  window.requestAnimationFrame(animate);
+}
+
+// Iniciar giro
+function spin() {
+  if (!pause) return;
+
+  winnerIndex = null; // Limpiar ganador anterior
+  if (blinkInterval) clearInterval(blinkInterval); // Detener parpadeo si hay
+  blinkState = false;
+
+  maxRotation = 360 * 2 + Math.random() * 360;
+  currentDeg = 0;
+  pause = false;
+  speed = 20;
+  draw();
+  window.requestAnimationFrame(animate);
+}
+
+function startBlinking() {
+  if (blinkInterval) clearInterval(blinkInterval); // Por si había otro
+
+  blinkState = false;
+  blinkInterval = setInterval(() => {
+    blinkState = !blinkState; // Cambiar entre dorado y original
+    draw();
+  }, 400); // Cambia cada 400 ms
+}
+
+// Primer dibujado
+draw();
