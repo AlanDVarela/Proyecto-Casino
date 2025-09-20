4ddd7eef162b601fec3a6aa93715b9a57e4d4f0e
Alan Varela <141166516+AlanDVarela@users.noreply.github.com>
2025-09-19 20:47:31 -0600

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 
 //Mongo
-let mongoConnection = "mongodb+srv://admin:Yosoypro1@myapp.ezop7xc.mongodb.net/Casino";
+let mongoConnection = "mongo";
 let db = mongoose.connection;
 
 mongoose.set('strictQuery', true); 
